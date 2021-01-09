1e0ac708c1f50634445bfde9305fb536c7d30a0f
wking <wkings@qq.com>
2021-01-09 14:59:46 +0800

    update

--- read_TDX_data.py
+++ read_TDX_data.py
@@ -4,8 +4,10 @@ import os
 import time
 from struct import unpack
  
- 
-# 将通达信的日线文件转换成CSV格式
+source = 'd:/stock/通达信'   #指定通达信目录
+target = 'd:/日线数据'  #指定数据保存目录
+
+# 将通达信的日线文件转换成CSV格式函数
 def day2csv(source_dir, file_name, target_dir):
     # 以二进制方式打开源文件
     source_file = open(source_dir + os.sep + file_name, 'rb')
@@ -33,16 +35,10 @@ def day2csv(source_dir, file_name, target_dir):
         begin += 32
         end += 32
     target_file.close()
- 
-
-
-sourcesh = 'd:/stock/通达信/vipdoc/sh/lday'
-sourcesz = 'd:/stock/通达信/vipdoc/sz/lday'
-target = 'd:/日线数据'
 
 #判断目录和文件是否存在，存在则直接删除
 if os.path.exists(target):
-    choose = input("文件已存在，是否覆盖？y/n")
+    choose = input("文件已存在，是否删除？ y/n ")
     if choose == 'y':
         for root, dirs, files in os.walk(target, topdown=False):
             for name in files:
@@ -52,22 +48,25 @@ if os.path.exists(target):
 else:
     os.mkdir(target)
 
-file_list = os.listdir(sourcesh)
+
+#处理沪市股票
+file_list = os.listdir(source + '/vipdoc/sh/lday')
 begintime = time.time()
 for f in file_list:
     if f[0:3] == 'sh6' or f[0:8] == 'sh999999': #处理沪市sh6开头和sh999999(上证指数)文件，否则跳过此次循环
-        day2csv(sourcesh, f, target)
+        day2csv(source + '/vipdoc/sh/lday', f, target)
         print(time.strftime("[%H:%M:%S] 处理 ", time.localtime()) + f)
     else:
         continue
 print('沪市处理完毕，用时' + str(time.time()-begintime))
 os.rename(target + '/999999.csv', target + '/上证指数.csv') 
 
-file_list = os.listdir(sourcesz)
+#处理深市股票
+file_list = os.listdir(source + '/vipdoc/sz/lday')
 begintime = time.time()
 for f in file_list:
     if f[0:4] == 'sz00' or f[0:4] == 'sz30':    #处理深市sh00开头和创业板sh30文件，否则跳过此次循环
-        day2csv(sourcesz, f, target)
+        day2csv(source + '/vipdoc/sz/lday', f, target)
         print(time.strftime("[%H:%M:%S] 处理 ", time.localtime()) + f)
     else:
         continue
