9d25f422048c592a76782921e696b9d4173a417d
wking <wkings@qq.com>
2021-01-11 00:31:26 +0800

    1.添加debug功能  2.添加附加最新数据功能

--- read_TDX_data.py
+++ read_TDX_data.py
@@ -7,34 +7,75 @@ from decimal import Decimal  #用于浮点数四舍五入
  
 source = 'd:/stock/通达信'   #指定通达信目录
 target = 'd:/日线数据'  #指定数据保存目录
+debug = 1   #是否开启调试日志输出  1开 0关
 
-# 将通达信的日线文件转换成CSV格式函数
+# debug输出函数
+def user_debug(print_str,print_value):
+    """第一个参数为变量名称，第二个参数为变量的值"""
+    if debug == 1:
+        print(str(print_str) + ' = ' + str(print_value))
+
+
+# 将通达信的日线文件转换成CSV格式函数。通达信数据文件32字节为一组。
 def day2csv(source_dir, file_name, target_dir):
+    """通达信数据文件转换为CSV格式函数"""
+
     # 以二进制方式打开源文件
-    source_file = open(source_dir + os.sep + file_name, 'rb')
-    buf = source_file.read()
+    source_path = source_dir + os.sep + file_name  #源文件包含文件名的路径
+    source_file = open(source_path, 'rb')
+    buf = source_file.read()  #读取源文件保存在变量中
     source_file.close()
- 
+    source_size = os.path.getsize(source_path)   #获取源文件大小
+    user_debug('源文件行数', int(source_size/32))
+
     # 打开目标文件，后缀名为CSV
-    target_file = open(target_dir + os.sep + file_name[2:-4] + '.csv', 'w')
-    buf_size = len(buf)
-    rec_count = buf_size // 32
-    begin = 0
-    end = 32
-    header = str('date') + ', ' + str('open') + ', ' + str('high') + ', ' + str('low') + ', ' \
+    target_path = target_dir + os.sep + file_name[2:-4] + '.csv'  #目标文件包含文件名的路径
+    #user_debug('target_path', target_path)
+    
+    if not os.path.isfile(target_path):
+        #目标文件不存在。写入表头行。begin从0开始转换
+        target_file = open(target_path, 'w', encoding="utf-8")  #以覆盖写模式打开文件
+        header = str('date') + ', ' + str('open') + ', ' + str('high') + ', ' + str('low') + ', ' \
         + str('close') + ', ' + str('amount') + ', ' + str('vol') +  ', ' \
-        + str('单位元，成交量股') + '\n'
-    target_file.write(header)
-    for i in range(rec_count):
+        + str('单位元、成交量股')
+        target_file.write(header)
+        begin = 0
+        end = begin + 32
+    else:
+        # 不为0，文件有内容。行附加。
+        # 通达信数据32字节为一组，因此通达信文件大小除以32可算出通达信文件有多少行（也就是多少天）的数据。
+        # 再用readlines计算出目标文件已有多少行（目标文件多了首行标题行），(行数-1)*32 即begin要开始的字节位置
+        
+        target_file = open(target_path, 'a+', encoding="utf-8")  #以追加读写模式打开文件
+        #target_size = os.path.getsize(target_path)  #获取目标文件大小
+
+        # 由于文件指针在文件的结尾，需要先把指针改到文件开头，读取文件行数。
+        user_debug('当前指针', target_file.tell())
+        target_file.seek(0,0)  #文件指针移到文件头
+        user_debug('移动指针到开头', target_file.seek(0,0))
+        row_number = len(target_file.readlines())  #获得文件行数
+        user_debug('文件总行数', row_number)
+        target_file.seek(0,2)  #文件指针移到文件尾
+        user_debug('移动指针到末尾', target_file.seek(0,2))
+
+        if row_number == 0:  #如果文件出错是0的特殊情况
+            begin = 0
+        else:
+            begin = (row_number - 1) * 32
+
+        end = begin + 32
+        print('追加模式，从' + str(row_number + 1) + '行开始')
+
+    while begin<source_size:
         # 将字节流转换成Python数据格式
         # I: unsigned int
         # f: float
         #a[5]浮点类型的成交金额，使用decimal类四舍五入为整数
         a = unpack('IIIIIfII', buf[begin:end])       
-        line = str(a[0]) + ', ' + str(a[1] / 100.0) + ', ' + str(a[2] / 100.0) + ', ' \
+        line = '\n' + str(a[0]) + ', ' + str(a[1] / 100.0) + ', ' + str(a[2] / 100.0) + ', ' \
             + str(a[3] / 100.0) + ', ' + str(a[4] / 100.0) + ', ' \
             + str(Decimal(a[5]).quantize(Decimal("1."), rounding = "ROUND_HALF_UP")) + ', ' \
-            + str(a[6]) + '\n'
+            + str(a[6])
         target_file.write(line)
         begin += 32
         end += 32
@@ -42,7 +83,7 @@ def day2csv(source_dir, file_name, target_dir):
 
 #判断目录和文件是否存在，存在则直接删除
 if os.path.exists(target):
-    choose = input("文件已存在，是否删除? (y/n) :")
+    choose = input("文件已存在，输入 y 删除现有文件并重新生成完整数据，其他输入则附加最新日期数据: ")
     if choose == 'y':
         for root, dirs, files in os.walk(target, topdown=False):
             for name in files:
@@ -57,20 +98,19 @@ file_list = os.listdir(source + '/vipdoc/sh/lday')
 begintime = time.time()
 for f in file_list:
     if f[0:3] == 'sh6' or f[0:8] == 'sh999999': #处理沪市sh6开头和sh999999(上证指数)文件，否则跳过此次循环
-        day2csv(source + '/vipdoc/sh/lday', f, target)
         print(time.strftime("[%H:%M:%S] 处理 ", time.localtime()) + f)
+        day2csv(source + '/vipdoc/sh/lday', f, target)
     else:
         continue
-print('沪市处理完毕，用时' + str(time.time()-begintime))
-os.rename(target + '/999999.csv', target + '/上证指数.csv') 
+print('沪市处理完毕，用时' + str(int(time.time()-begintime)) + '秒')
 
 #处理深市股票
 file_list = os.listdir(source + '/vipdoc/sz/lday')
 begintime = time.time()
 for f in file_list:
     if f[0:4] == 'sz00' or f[0:4] == 'sz30':    #处理深市sh00开头和创业板sh30文件，否则跳过此次循环
-        day2csv(source + '/vipdoc/sz/lday', f, target)
         print(time.strftime("[%H:%M:%S] 处理 ", time.localtime()) + f)
+        day2csv(source + '/vipdoc/sz/lday', f, target)
     else:
         continue
-print('深市处理完毕，用时' + str(time.time()-begintime))
+print('深市处理完毕，用时' + str(int(time.time()-begintime)) + '秒')
