021b69c4ad2831ce9bbd2c85ab5170e1fb5af8db
wking <wkings@qq.com>
2021-01-13 14:31:20 +0800

    update

--- module.akshare.py
+++ module.akshare.py
@@ -7,21 +7,78 @@
 
 作者：wking [http://wkings.net]
 """
+import os
 import pandas as pd
-import csv
 
-import akshare as ak
+import akshare
 
 import user_config as ucfg
 
-def 
+
 # 获取沪深 A 股股票代码和简称数据
-stock_list = ak.stock_info_a_code_name()
-df = pd.DataFrame(stock_list)
-df.to_csv(ucfg.csv_path + '/123.csv')
+def download_stocklist(): 
+    """
+    调用akshare库获取当前最新A股股票列表，返回列表类型的代码
+    """
+    df = pd.DataFrame(akshare.stock_info_a_code_name())
+    stocklist = df['code'].tolist()
+    return stocklist
 
-f_obj = open(ucfg.csv_path + '/123.csv', 'r', encoding="utf-8")  # 以覆盖写模式打开文件
-f_obj.close()
+# 切片股票列表stocklist，指定开始股票和结束股票
+def update_stocklist(stocklist, start_num, end_num):
+    """
+    
 
+    Parameters
+    ----------
+    start_num : str
+    从哪只股票开始处理
+    
+    end_num : str
+    处理到哪只股票为止
 
-print(stocklist)
+    Returns
+    -------
+    处理后的股票代码列表
+
+    """
+    for i in stocklist:
+        if i == start_num:
+            start_index = stocklist.index(i)
+            stocklist = stocklist[start_index:]
+        if i == end_num:
+            end_index = stocklist.index(i)
+            stocklist = stocklist[:end_index]
+            
+    return stocklist
+
+def download_stockdata():
+    for i in stocklist:
+        if i[0:1] == '6':
+            ii = 'sh' + i
+        elif i[0:1] == '0' or i[0:1] == '3':
+            ii = 'sz' + i
+        
+        process_info = '[' + str(stocklist.index(i) + 1) + '/' + str(len(stocklist)) + '] ' + i
+        try:
+            df = akshare.stock_zh_a_daily(symbol=ii, start_date="19901219", end_date="20210113", adjust="qfq")
+            csv_path = ucfg.csv_path + os.sep + i + '.csv'
+            df.to_csv(csv_path)
+        except:
+            print(process_info + ' >>>wrong<<<')
+        else:
+            print(process_info + ' done')
+    
+    print('处理完毕')
+
+# 主程序开始
+            
+# 下载最新股票代码列表            
+stocklist = download_stocklist()
+
+#定义要下载的股票区间
+start_stock_num = ''  # 留空则从头开始处理 不需要输入sh/sz
+end_stock_num = ''  # 留空则处理到末尾
+stocklist = update_stocklist(stocklist, start_stock_num, end_stock_num)
+
+download_stockdata()
