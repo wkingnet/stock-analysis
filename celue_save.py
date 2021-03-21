60dfcbdfd87fd40a003e740251612223520c6992
wking <wkings@qq.com>
2021-03-21 20:26:13 +0800

    加入剔除股票代码

--- celue_save.py
+++ celue_save.py
@@ -15,6 +15,11 @@ import func_TDX
 import user_config as ucfg
 
 
+# 变量定义
+要剔除的通达信概念 = ["ST板块", ]  # list类型。通达信软件中查看“概念板块”。
+要剔除的通达信行业 = ["T1002", ]  # list类型。记事本打开 通达信目录\incon.dat，查看#TDXNHY标签的行业代码。
+
+
 def celue_save(file_list, HS300_信号, tqdm_position=None):
     # print('\nRun task (%s)' % os.getpid())
     starttime_tick = time.time()
@@ -23,10 +28,10 @@ def celue_save(file_list, HS300_信号, tqdm_position=None):
         tq = tqdm(file_list)
     else:
         tq = tqdm(file_list, leave=False, position=tqdm_position)
-    for filename in tq:
-        tq.set_description(filename[:-4])
-        # process_info = f'[{(file_list.index(filename) + 1):>4}/{str(len(file_list))}] {filename}'
-        pklfile = ucfg.tdx['pickle'] + os.sep + filename
+    for stockcode in tq:
+        tq.set_description(stockcode)
+        # process_info = f'[{(stocklist.index(stockcode) + 1):>4}/{str(len(stocklist))}] {stockcode}'
+        pklfile = ucfg.tdx['pickle'] + os.sep + stockcode + ".pkl"
         df = pd.read_pickle(pklfile)
         if 'del' in sys.argv[1:]:
             if 'celue_buy' in df.columns:
@@ -53,10 +58,10 @@ def celue_save(file_list, HS300_信号, tqdm_position=None):
             df.loc[start_date:end_date, 'celue_buy'] = celue2
             df.loc[start_date:end_date, 'celue_sell'] = celue_sell
             df.reset_index(drop=True, inplace=True)
-            df.to_csv(ucfg.tdx['csv_lday'] + os.sep + filename[:-4] + '.csv', index=False, encoding='gbk')
-            df.to_pickle(ucfg.tdx['pickle'] + os.sep + filename)
-        lefttime_tick = int((time.time() - starttime_tick) / (file_list.index(filename) + 1)
-                            * (len(file_list) - (file_list.index(filename) + 1)))
+            df.to_csv(ucfg.tdx['csv_lday'] + os.sep + stockcode + '.csv', index=False, encoding='gbk')
+            df.to_pickle(ucfg.tdx['pickle'] + os.sep + stockcode + ".pkl")
+        lefttime_tick = int((time.time() - starttime_tick) / (file_list.index(stockcode) + 1)
+                            * (len(file_list) - (file_list.index(stockcode) + 1)))
 
         # 提取celue是true的列，单独保存到一个df，返回这个df
         df_celue = df_celue.append(df.loc[df['celue_buy'] | df['celue_sell']])
@@ -73,28 +78,52 @@ if __name__ == '__main__':
     df_hs300['date'] = pd.to_datetime(df_hs300['date'], format='%Y-%m-%d')  # 转为时间格式
     df_hs300.set_index('date', drop=False, inplace=True)  # 时间为索引。方便与另外复权的DF表对齐合并
     HS300_信号 = CeLue.策略HS300(df_hs300)
-    file_list = os.listdir(ucfg.tdx['pickle'])
+    print("生成股票列表")
+    stocklist = [i[:-4] for i in os.listdir(ucfg.tdx['pickle'])]
+    print(f'共 {len(stocklist)} 只股票')
+    print("剔除通达信概念股票")
+    tmplist = []
+    df = func_TDX.get_TDX_blockfilecontent("block_gn.dat")
+    # 获取df中blockname列的值是ST板块的行，对应code列的值，转换为list。用filter函数与stocklist过滤，得出不包括ST股票的对象，最后转为list
+    for i in 要剔除的通达信概念:
+        tmplist = tmplist + df.loc[df['blockname'] == i]['code'].tolist()
+    stocklist = list(filter(lambda i: i not in tmplist, stocklist))
+    print("剔除通达信行业股票")
+    tmplist = []
+    df = pd.read_csv(ucfg.tdx['tdx_path'] + os.sep + 'T0002' + os.sep + 'hq_cache' + os.sep + "tdxhy.cfg",
+                     sep='|', header=None, dtype='object')
+    for i in 要剔除的通达信行业:
+        tmplist = tmplist + df.loc[df[2] == i][1].tolist()
+    stocklist = list(filter(lambda i: i not in tmplist, stocklist))
+    print("剔除科创板股票")
+    tmplist = []
+    for stockcode in stocklist:
+        if stockcode[:2] != '68':
+            tmplist.append(stockcode)
+    stocklist = tmplist
+    股票总数 = len(stocklist)
+    print(f'共 {股票总数} 只候选股票')
 
     if 'del' in sys.argv[1:]:
         print(f'检测到参数 del, 完全重新生成策略信号')
 
     if 'single' in sys.argv[1:]:
         print(f'检测到参数 single, 单进程执行')
-        df_celue = celue_save(file_list, HS300_信号)
+        df_celue = celue_save(stocklist, HS300_信号)
     else:
         # 多线程。好像没啥效果提升
         # threads = []
         # t_num = 4  # 线程数
         # for i in range(0, t_num):
-        #     div = int(len(file_list) / t_num)
-        #     mod = len(file_list) % t_num
+        #     div = int(len(stocklist) / t_num)
+        #     mod = len(stocklist) % t_num
         #     if i+1 != t_num:
         #         # print(i, i * div, (i + 1) * div)
-        #         threads.append(threading.Thread(target=celue_save, args=(file_list[i*div:(i+1)*div], HS300_信号)))
+        #         threads.append(threading.Thread(target=celue_save, args=(stocklist[i*div:(i+1)*div], HS300_信号)))
         #     else:
         #         # print(i, i * div, (i + 1) * div + mod)
-        #         threads.append(threading.Thread(target=celue_save, args=(file_list[i*div:(i+1)*div+mod], HS300_信号)))
-        # # celue_save(file_list, HS300_信号)
+        #         threads.append(threading.Thread(target=celue_save, args=(stocklist[i*div:(i+1)*div+mod], HS300_信号)))
+        # # celue_save(stocklist, HS300_信号)
         #
         # print(threads)
         # for t in threads:
@@ -113,16 +142,16 @@ if __name__ == '__main__':
         p = Pool(processes=t_num, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),))
         pool_result = []  # 存放pool池的返回对象列表
         for i in range(0, t_num):
-            div = int(len(file_list) / t_num)
-            mod = len(file_list) % t_num
+            div = int(len(stocklist) / t_num)
+            mod = len(stocklist) % t_num
             if i + 1 != t_num:
                 # print(i, i * div, (i + 1) * div)
-                pool_result.append(p.apply_async(celue_save, args=(file_list[i * div:(i + 1) * div], HS300_信号, i)))
+                pool_result.append(p.apply_async(celue_save, args=(stocklist[i * div:(i + 1) * div], HS300_信号, i)))
             else:
                 # print(i, i * div, (i + 1) * div + mod)
                 pool_result.append(
-                    p.apply_async(celue_save, args=(file_list[i * div:(i + 1) * div + mod], HS300_信号, i)))
-        # celue_save(file_list, HS300_信号)
+                    p.apply_async(celue_save, args=(stocklist[i * div:(i + 1) * div + mod], HS300_信号, i)))
+        # celue_save(stocklist, HS300_信号)
 
         # print('Waiting for all subprocesses done...')
         p.close()
